8c9b89266035a75bc939a82f0d7c5c755b7a1d23
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-10 14:04:28 -0700

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,15 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d", n);
-if (n == 0)
-{
-printf("is zero");
-}
-else if (n > 0)
-{
+if (n > 0)
 printf("is positive");
-}
+
+else if (n == 0)
+printf("is zero");
+
 else
-{
 printf("is negative");
-}
+
 printf("\n");
 return (0);
 }
