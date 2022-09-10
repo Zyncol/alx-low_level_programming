9dfda512849293cf37fa1d9c848cdee36df7c3a6
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-10 15:06:07 -0700

    print out the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,18 @@ int d;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 d = n % 10;
-printf("Last digit of %d\n is %d", n, d);
+printf(" Last digit of %d\n is %d", n, d);
 if (d > 5)
 {
-printf("and is greater than 5");
+printf(" and is greater than 5");
 }
 else if (d == 0)
 {
-printf("and is zero");
+printf(" and is zero");
 }
 else if (d < 6 && d != 0)
 {
-printf("and is less than 6 and not 0");
+printf(" and is less than 6 and not 0");
 }
 printf("\n");
 return (0);
