a8867742379088b5a0a155767c17bc70840ac746
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-11 04:18:04 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry point
+*Descriptio: print numbers
+*Return: 0 (success0
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+printf("%d", n);
+putchar('\n');
+return (0);
+}
