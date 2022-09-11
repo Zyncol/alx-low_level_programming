df831dca4ac13310c26ea5062e1084e7a6ef5076
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-11 14:09:29 -0700

    comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry point
+*Return: 0 (success)
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = 48; a <= 56; a++)
+{
+for (b = a + 1; b < 57; b++)
+{
+putchar((char)a);
+putcha((char)b);
+if (a != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
