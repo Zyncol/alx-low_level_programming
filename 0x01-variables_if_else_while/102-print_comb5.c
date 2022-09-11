c8159fe332340760559060483d710751a9127048
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-11 15:15:49 -0700

    combination 5 tought

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry point
+*Return: 0 (success)
+*/
+int main(void)
+{
+int a;
+int b;
+for (a = 0; a <= 98; a++)
+{
+for (b = a + 1; b <= 99; b++)
+{
+if (a < b)
+{
+putchar(a / 10 + '0');
+putchar(a % 10 + '0');
+putchar(' ');
+putchar(b / 10 + '0');
+putchar(b % 10 + '0');
+if (a != 98 && b != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
