894312a119dc95543abc84ee442b0fb739f0dc6f
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-13 16:19:29 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - prints alphabet in lower cases
+*Return: void
+*/
+void print_alphabet(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+_putchar('\n');
+c++;
+}
+}
