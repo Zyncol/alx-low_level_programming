7c2fbdfeeced55d3669eaf12d5b8b5fe6febac1b
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-13 18:57:54 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_last_digit - prints the last digit
+*@m: the parameter
+*Return: the last digit of a figure
+*/
+int print_last_digit(int m)
+{
+int lastdigit;
+lastdigit = m % 10;
+return (lastdigit);
+}
