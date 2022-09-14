441ef7b02fb3cf7cfedbecc81e74fb998caa56ab
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-13 18:46:26 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_abs - prints an absolute value of an integer
+*Return: Always 0
+*@n: our parameter
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (-1 * n);
+else
+return (n);
+}
