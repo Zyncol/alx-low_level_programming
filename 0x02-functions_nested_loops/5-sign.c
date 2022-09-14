e9ac287161465f0b3f160feae3cde175d9fd81da
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-13 18:21:18 -0700

    result of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_sign - prints out the result
+*@n: we will use this parameter
+*Return: 1 if it is positive, 0 if nuetral else -1 if negative
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
