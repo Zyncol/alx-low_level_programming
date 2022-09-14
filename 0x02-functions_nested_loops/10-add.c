48c5cd0041b50cc85587b84ace9fd23718fa44d7
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-14 13:22:05 -0700

    adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*add - sum app the values of the parameters
+*Return: the sum
+*@a: first parameter
+*@b:second parameter
+*/
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
