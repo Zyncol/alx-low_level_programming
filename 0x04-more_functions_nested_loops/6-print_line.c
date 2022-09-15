8ac389c4d79e20dfd22fd7ed94ee7c2337200d0d
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-15 14:56:28 -0700

    a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line - printing a line
+*@n: a character to be
+*Return: void
+*/
+void print_line(int n)
+{
+int k;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (k = 0; k <= n; k++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
