f276212ecf7d2f363abdb1f5bb8ce371dfd92e16
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-15 13:24:27 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*mul - it multiplies given integers
+*@a: a cvariable to be used
+*@b: a variable to be used
+*Return: the product
+*/
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}
