a30b81b5fc5fc477e224d8fe310e4c18eaa71f26
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-15 18:11:40 -0700

    prime1

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,9 +5,9 @@
 */
 int main(void)
 {
-sunsigned long int l;
+unsigned long int l;
 unsigned long int u = 612852475143;
-for (l = 3; l < 1205; l += 2)
+for (l = 3; l < 12057; l += 2)
 {
 while (u % l == 0 && u != l)
 u /= l;
