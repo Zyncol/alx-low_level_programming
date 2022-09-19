066bcc85eeadaf9c1739c1e56bb0ae45d8560f41
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-19 16:58:48 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_strcpy - copies the string pointed to
+*@dest: to be used
+*@src: to be used
+*Return:  the pointer
+*/
+char *_strcpy(char *dest, char *src)
+{
+int f;
+for (f = 0; src[f] != '\0'; f++)
+dest[f] = src[f];
+return (&dest[0]);
+}
