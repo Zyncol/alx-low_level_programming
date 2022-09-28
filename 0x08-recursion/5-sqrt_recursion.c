c1714edc1ecea4e772cfbdf8cecfa852162994f5
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-28 03:47:32 -0700

    sqaure root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,20 +6,20 @@
 */
 int _sqrt_recursion(int n)
 {
-return (square(n - 1));
+return (_square(n - 1));
 }
 /**
-*square - finds the square root number
+*_square - finds the square root number
 *@n: number to be used to find root
 *@m: checks
 *Return: the results
 */
-int square(int n, int m)
+int _square(int n, int m)
 {
 if (m * m > n)
 return (-1);
 else if (m * m == n)
 return (m);
 else
-return (square(n, m + 1));
+return (_square(n, m + 1));
 }
