1f79abe33f4eef5c1a903ff3bca8d7ee6c719ba3
Zyncol <isaacyilipochipofya@gmail.com>
2022-09-30 16:46:36 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - prints the name of a program
+*@argc: argument count
+*@argv: argument array
+*Return: 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+printf("The program name is: %s\n", argv[0]);
+return (0);
+}
