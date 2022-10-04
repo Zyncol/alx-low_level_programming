74d7bff032599ebf93d607bf71e7a93d56fd0a1a
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-04 16:31:51 -0700

    frees

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+/**
+*free_grid - frees a dimensional grid
+*@grid: array to free
+*@height: height
+*/
+void free_grid(int **grid, int height)
+{
+int x;
+for (x = 0; x < height; x++)
+free(grid[x]);
+free(grid);
+}
