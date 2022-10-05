070e093d6383700ceda71545605e0423cda409dc
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-04 17:20:10 -0700

    dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 int **m, x, y;
-if (width < 0 || height < 0)
+if (width <= 0 || height <= 0)
 return (NULL);
 m = (int **)malloc(sizeof(int *) * height);
 if (m == NULL)
