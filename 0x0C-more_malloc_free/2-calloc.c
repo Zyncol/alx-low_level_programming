8614931e75dffeb9cff3ba2b46b35980509786af
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-06 17:03:01 -0700

    allocate memory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_calloc - allocates memory for an array
+*@nmemb: number of elements
+*@size: the size
+*Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int n;
+char *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+return (NULL);
+for (n = 0; n < (nmemb * size); n++)
+ptr[n] = 0;
+return (ptr);
+}
