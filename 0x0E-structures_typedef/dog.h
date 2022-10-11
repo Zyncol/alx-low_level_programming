6bdea987533645c1ec0a75a764357afd3ede6935
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-11 14:37:41 -0700

    main file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,23 @@
 #ifndef _DOG_H_
 #define _DOG_H
 /**
- *struct dog - dog structure attributes
- *@name: name of the dog
- *@age: age of the dog
- *@owner: the owner of the dog
- */
+*struct dog - dog structure attributes
+*@name: name of the dog
+*@age: age of the dog
+*@owner: the owner of the dog
+*/
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 /**
- *dog_t - typedef for struct dog
- */
+*dog_t - typedef for struct dog
+*/
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
