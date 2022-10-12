4b1495ced41b5a6abbd659b8cf46d8789be557cc
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-12 04:09:51 -0700

    search for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+*int_index - search for an integre
+*@array: an array to be searched
+*@size: size of an array
+*@cmp: a pointer
+*Return: returns the index of an element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int index;
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+for (index = 0; index < size; ++index)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+return (-1);
+}
