8bc3f681ab055f85befa48d7ef6fc782cc031d16
Zyncol <isaacyilipochipofya@gmail.com>
2022-10-20 06:49:04 -0700

    adds a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+*add_node - adds a node at the beginning of a node
+*@head: the head of a lined list
+*@str: string to store
+*Return: the address of the head
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_node;
+size_t n;
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+new_node->str = strdup(str);
+for (n = 0; str[n]; n++)
+;
+new_node->len = n;
+new_node->next = *head;
+*head = new_node;
+return (*head);
+}
