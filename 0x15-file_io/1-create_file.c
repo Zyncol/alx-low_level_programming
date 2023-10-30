d7cf071f2ec87e3b076834b147f294be2f4cc6d8
Zyncol <isaacyilipochipofya@gmail.com>
2023-10-30 16:45:54 -0700

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int zen;
+int wr;
+int nletters;
+if (!filename)
+return (-1);
+zen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (zen == -1)
+return (-1);
+if (!text_content)
+text_content = "";
+for (nletters = 0; text_content[nletters]; nletters++)
+;
+wr = write(fd, text_content, nletters);
+if (wr == -1)
+return (-1);
+close(zen);
+return (1);
+}
